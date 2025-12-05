0d735c6ded4a7f963d1b68ad4a17516392f6143e
UELINTON QUINTAO SILVERIO <00007386206@rondonia.local>
2025-12-05 10:36:15 -0400

    fix: redirect logout to login page

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="nsp/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("", include("core.urls")),
 ]
